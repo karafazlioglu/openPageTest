1f2f6b09a171466ccd9dc55990aeb4d294959887
muhammet <muhammetkarafazlioglu@gmail.com>
2023-02-24 22:46:59 +0300

    test project added

---
+++ src/test/java/OpenPageTest.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class OpenPageTest {
+
+
+    public WebDriver driver;
+    protected static String url = "https://www.google.com/";
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void correctOpen() {
+        driver.get(url);
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
