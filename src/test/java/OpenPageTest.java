8d30510953ff3da97e3241cb0ab178e074783599
muhammet <muhammetkarafazlioglu@gmail.com>
2023-03-16 17:26:53 +0300

    comments added to the second class

--- src/test/java/OpenPageTest.java
+++ src/test/java/OpenPageTest.java
@@ -38,34 +38,39 @@ public class OpenPageTest {         // openpagetest adında bir sınıf açılı
         //Assert.assertEquals(driver.getTitle(), "NetWork");            // sayfa başlığının network olduğu test edilir
         //Assert.assertEquals(driver.getCurrentUrl(), "https://www.network.com.tr/");       // sayfa url inin başta url stringine atadığımız url olduğu test edilir
 
-        WebElement arKutu = driver.findElement(By.xpath("//input[@name='searchKey']"));
-        arKutu.sendKeys("ceket");
-        arKutu.submit();
+        WebElement arKutu = driver.findElement(By.xpath("//input[@name='searchKey']"));     // arama kurusu web elementi driverda bulunur
+        arKutu.sendKeys("ceket");       // elementin içine ceket yazılır
+        arKutu.submit();        // enter a tıklanır
 
-        WebElement dahaFazla = driver.findElement(By.xpath("//button[@class='button -secondary -sm relative']"));
-        Actions action = new Actions(driver);
-
+        WebElement dahaFazla = driver.findElement(By.xpath("//button[@class='button -secondary -sm relative']"));           // daha fazla göster butonunun sayfadaki html kodundan xpath i alınır
+        Actions action = new Actions(driver);           // daha fazla kutusuna tıklanır
         action.click(dahaFazla);
 
 
-        WebElement hover = driver.findElement(By.xpath("(//div[@data-order='3'])[1]"));
+        WebElement hover = driver.findElement(By.xpath("(//div[@data-order='3'])[1]"));     // ilk indirlimli cekete hover edilir
         action.moveToElement(hover).build().perform();
 
-        WebElement sizeCl = driver.findElement(By.xpath("//label[@extcode='1083770004']"));
+        WebElement sizeCl = driver.findElement(By.xpath("//label[@extcode='1083770004']"));     // beden seçimi yapılır
         action.click(sizeCl);
 
-        WebElement basket = driver.findElement(By.xpath("//button[@class='header__basketTrigger js-basket-trigger -desktop']"));
+        WebElement basket = driver.findElement(By.xpath("//button[@class='header__basketTrigger js-basket-trigger -desktop']"));        // sepetim'e gidilir
         action.click(basket);
 
-        WebElement newPrice = driver.findElement(By.xpath("//span[@class='a-product__price -salePrice']"));
+        WebElement newPrice = driver.findElement(By.xpath("//span[@class='a-product__price -salePrice']"));     // indirimli fiyat np stringine depolanır
         String np = newPrice.getText();
         System.out.println(np);
 
-        WebElement oldPrice = driver.findElement(By.xpath("//span[@class='a-product__price -oldPrice -labelPrice']"));
+        WebElement oldPrice = driver.findElement(By.xpath("//span[@class='a-product__price -oldPrice -labelPrice']"));      // ham fiyat op stringine depolanır
         String op = oldPrice.getText();
         System.out.println(op);
 
-        WebElement go = driver.findElement(By.xpath("//a[@class='button -primary header__basket--checkout header__basketModal -checkout']"));
+        // TODO: 16.03.2023 indirimli fiyatın eski fiyattan düşük olduğunun kontrolü
+
+
+
+
+
+        WebElement go = driver.findElement(By.xpath("//a[@class='button -primary header__basket--checkout header__basketModal -checkout']"));   // devam et butonuna tıklanır
         action.click(go);
 
         //WebElement cont = driver.findElement(By.xpath("//a[@class='button -primary header__basket--checkout header__basketModal -checkout']"));
